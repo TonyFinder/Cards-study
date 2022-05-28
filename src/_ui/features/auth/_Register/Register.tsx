a2e391729b5e25383372fb5ee34d1ec5859da76d
Oleg655 <savinoleg95@gmail.com>
2022-05-28 21:13:54 +0300

    create inputs

--- src/_ui/features/auth/_Register/Register.tsx
+++ src/_ui/features/auth/_Register/Register.tsx
@@ -1,7 +1,36 @@
-import styles from '../auth.module.scss'
+import axios from "axios";
+import { ChangeEvent, useEffect, useState } from "react";
+import styles from "../auth.module.scss";
+
+export const instance = axios.create({
+  baseURL: process.env.REACT_APP_BACK_URL || "http://localhost:7542/2.0/",
+  withCredentials: true,
+});
 
 export const Register = () => {
-  return <div className={styles.container}>
+  // useEffect(() => {
+  //   instance.post("auth/register", { email, password });
+  // }, []);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  return (
+    <div className={styles.container}>
       Register
-  </div>
-}
+      <div>
+        <input
+          value={email}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setEmail(e.currentTarget.value);
+          }}
+        />
+        <input
+          value={password}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setPassword(e.currentTarget.value);
+          }}
+        />
+        <button onClick={()=>{instance.post("auth/register", { email, password })}}>Register</button>
+      </div>
+    </div>
+  );
+};
