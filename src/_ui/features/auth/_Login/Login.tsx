cd1e4e433f05994b5be53202e3dd92426e1867f5
Red <amekysw@gmail.com>
2022-05-29 17:09:22 +0300

    login error handler and style

--- src/_ui/features/auth/_Login/Login.tsx
+++ src/_ui/features/auth/_Login/Login.tsx
@@ -1,9 +1,9 @@
-import styles from '../auth.module.scss'
+import styles from './authVadim.module.scss'
 import {Input} from "../../../common/_superComponents/Input/Input";
 import {Button} from "../../../common/_superComponents/Button/Button";
 import {Checkbox} from "../../../common/_superComponents/Checkbox/Checkbox";
 import {useState} from "react";
-import {Navigate} from 'react-router-dom';
+import {Link, Navigate} from 'react-router-dom';
 import {useAppDispatch, useCustomSelector} from "../../../../_bll/main/store";
 import {setDataUser} from "../../../../_bll/features/profile/profileReducer";
 import {loginInitialStateType} from "../../../../_bll/features/auth/_login/loginReducer";
@@ -14,19 +14,24 @@ export const Login = () => {
     const dispatch = useAppDispatch()
     const [email, setEmail] = useState('nya-admin@nya.nya');
     const [password, setPassword] = useState('1qazxcvBG');
+    const [checkbox, setCheckbox] = useState(false);
 
 
     const onChangeEmailHandler = (value: string) => {
         setEmail(value)
     }
+
     const onChangePasswordHandler = (value: string) => {
         setPassword(value)
     }
 
-    const onClickLogin = () => {
-        dispatch(setDataUser(email, password, false))
+    const onChangeCheckboxHandler = () => {
+        setCheckbox(!checkbox)
     }
 
+    const onClickLogin = () => {
+        dispatch(setDataUser(email, password, checkbox))
+    }
 
     if (isLoggedIn) {
         return <Navigate to='/profile'/>
@@ -37,20 +42,23 @@ export const Login = () => {
             <div className={styles.header}>
                 <h1>It-incubator</h1>
                 <h3>Sign in</h3>
-            </div>
-            <div className={styles.error}>{error && error}</div>
-            <div className={styles.body}>
-                <label>Email</label>
-                <Input onChangeText={onChangeEmailHandler} value={email} placeholder="Email"/>
-                <label>Password</label>
-                <Input onChangeText={onChangePasswordHandler} value={password} placeholder="Password"/>
+                <div className={styles.error}>{error && error}</div>
+                <div>
+                    <label>Email</label>
+                    <Input onChangeText={onChangeEmailHandler} value={email} placeholder='Email'/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <Input onChangeText={onChangePasswordHandler} value={password} type='password'
+                           placeholder='Password'/>
+                </div>
                 <span>Forgot password</span>
-                <Checkbox>Remember me </Checkbox>
+                <Checkbox onClick={onChangeCheckboxHandler} checked={checkbox}>Remember me </Checkbox>
             </div>
             <div className={styles.footer}>
                 <Button onClick={onClickLogin}>Login</Button>
                 <span>Don’t have an account?</span>
-                <a>Sign Up</a>
+                <Link to='/register'>Sign Up</Link>
             </div>
         </div>
     )
