d4a4c2eadfc8e67f0f1f8a80c38cfcd93190865d
Oleg655 <savinoleg95@gmail.com>
2022-05-30 10:39:58 +0300

    create confirm password

--- src/_ui/features/auth/_Register/Register.tsx
+++ src/_ui/features/auth/_Register/Register.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { ThunkDispatch } from "redux-thunk";
@@ -29,20 +29,52 @@ export const Register = () => {
     RegisterActionTypes
   > = useDispatch();
   const [email, setEmail] = useState<string>("");
+  const [emailError, setEmailError] = useState<boolean>(false);
   const [password, setPassword] = useState<string>("");
+  const [errorPssword, setErrorPssword] = useState<boolean>(false);
+  const [repeatPassword, setRepeatPassword] = useState<string>("");
+  const [errorRepeatPssword, setErrorRepeatPssword] = useState<boolean>(false);
+
   const error = useCustomSelector<string>((state) => state.register.error);
   const isRegistered = useCustomSelector<boolean>(
     (state) => state.register.isRegistered
   );
-  if (isRegistered) {
+  if (isRegistered && repeatPassword === password) {
     navigate("/login");
   }
   return (
     <div className={styles.formContainer}>
       <div className={styles.form}>
         Sign Up
-        <Input value={email} onChangeText={setEmail} />
-        <Input type={'password'} value={password} onChangeText={setPassword} />
+        <div>
+          <label>Email</label>
+          <Input
+            value={email}
+            onChangeText={setEmail}
+            error={emailError}
+            onChangeError={setEmailError}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <Input
+            type={"password"}
+            value={password}
+            onChangeText={setPassword}
+            error={errorPssword}
+            onChangeError={setErrorPssword}
+          />
+        </div>
+        <div>
+          <label>Confirm password</label>
+          <Input
+            type={"password"}
+            value={repeatPassword}
+            onChangeText={setRepeatPassword}
+            error={errorRepeatPssword}
+            onChangeError={setErrorRepeatPssword}
+          />
+        </div>
         <Button
           onClick={() => {
             dispatch(requestRegistration({ email, password }));
