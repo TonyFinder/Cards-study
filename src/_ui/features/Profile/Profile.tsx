39f4e04ddc77471e823ce756daca1761a2e95d82
Anton <antonrozdobudko@gmail.com>
2022-05-30 16:07:48 +1000

    api, appReducer, profileReducer and store have been changed

--- src/_ui/features/Profile/Profile.tsx
+++ src/_ui/features/Profile/Profile.tsx
@@ -1,24 +1,29 @@
 import styles from './Profile.module.scss'
 import {Input} from '../../common/_superComponents/Input/Input';
 import {Button} from '../../common/_superComponents/Button/Button';
-import {useCustomSelector} from '../../../_bll/main/store';
+import {useAppDispatch, useCustomSelector} from '../../../_bll/main/store';
 import {useState} from 'react';
+import {changeProfileDataTC} from '../../../_bll/features/profile/profileReducer';
+import {AuthDataType} from '../../../_dal/api-profile';
 
 export const Profile = () => {
-    const nickName = useCustomSelector<string>(state => state.profile.name)
-    const email = useCustomSelector<string>(state => state.profile.email)
-    const image = useCustomSelector<string>(state => state.profile.avatar ? state.profile.avatar : '')
+    const {name, email, avatar} = useCustomSelector<AuthDataType>(state => state.profile)
+    let dispatch = useAppDispatch()
 
-    const [nickNameValue, setNickNameValue] = useState<string>(nickName)
+    const [nickNameValue, setNickNameValue] = useState<string>(name)
     const [emailValue, setEmailValue] = useState<string>(email)
     const [errorNickName, setErrorNickName] = useState<boolean>(false)
     const [errorEmailValue, setErrorEmailValue] = useState<boolean>(false)
 
     const saveButtonDisable = !nickNameValue || !emailValue || errorNickName || errorEmailValue
 
+    const changeProfileData = () => {
+        dispatch(changeProfileDataTC(nickNameValue, 'http://amintl.com.pk/wp-content/uploads/2019/11/avatar3.png'))
+    }
+
     return <div className={styles.container}>
         <h2>Personal Information</h2>
-        <img src={image} alt={'avatar'}/>
+        <img src={avatar} alt={'avatar'}/>
         <Input
             value={nickNameValue}
             onChangeText={setNickNameValue}
@@ -30,7 +35,7 @@ export const Profile = () => {
             error={errorEmailValue}
             onChangeError={setErrorEmailValue}/>
         <Button>Cancel</Button>
-        <Button disabled={saveButtonDisable}>Save</Button>
+        <Button disabled={saveButtonDisable} onClick={changeProfileData}>Save</Button>
 
     </div>
 }
