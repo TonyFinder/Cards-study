c4489ea1aebece1939b981d5e13a0bdb3ae9ea6a
Anton <antonrozdobudko@gmail.com>
2022-06-14 10:08:13 +1000

    SetPass page. Changed structure, logic, validation and scss from template. Refactoring for SetPass reducer as well.

--- src/_bll/features/auth/setPass/setPassReducer.ts
+++ src/_bll/features/auth/setPass/setPassReducer.ts
@@ -1,54 +1,38 @@
 import { Dispatch } from "redux";
-import {
-  FieldSetPassword,
-  setPasswordApi,
-} from "../../../../_dal/api-setPassword";
+import { FieldSetPassword, setPasswordApi } from "../../../../_dal/api-setPassword";
 
 let initialState = {
   info: "",
   error: "",
 };
 
-export const setPassReducer = (
-  state: InitialStateType = initialState,
-  action: SetPassActionTypes
-): InitialStateType => {
+export const setPassReducer = (state: SetPasswordInitialStateType = initialState, action: SetPassActionTypes): SetPasswordInitialStateType => {
   switch (action.type) {
     case "SET-PASSWORD-SUCCESS":
-      return {
-        ...state,
-        info: action.info,
-      };
+      return {...state, info: action.info}
     case "SET-ERROR":
-      return {
-        ...state,
-        error: action.error,
-      };
+      return {...state, error: action.error}
     default:
-      return state;
+      return state
   }
-};
+}
 
 // actions
-
 const setError = (error: string) => ({ type: "SET-ERROR", error } as const);
-const setPasswordSuccsess = (info: string) =>
-  ({ type: "SET-PASSWORD-SUCCESS", info } as const);
+const setPasswordSuccess = (info: string) => ({ type: "SET-PASSWORD-SUCCESS", info } as const);
 
 // thunks
 export const setNewPassword = (data: FieldSetPassword) => {
   return async (dispatch: Dispatch<SetPassActionTypes>) => {
     try {
-      const response = await setPasswordApi.setPassword(data);
-      dispatch(setPasswordSuccsess(response.data.info))
+      const response = await setPasswordApi.setPassword(data)
+      dispatch(setPasswordSuccess(response.data.info))
     } catch (AxiosError: any) {
-      dispatch(setError(AxiosError.response.data.error));
+      dispatch(setError(AxiosError.response.data.error))
     }
-  };
-};
+  }
+}
 
 // types
-export type SetPassActionTypes =
-  | ReturnType<typeof setError>
-  | ReturnType<typeof setPasswordSuccsess>;
-type InitialStateType = typeof initialState;
+export type SetPassActionTypes = ReturnType<typeof setError> | ReturnType<typeof setPasswordSuccess>
+export type SetPasswordInitialStateType = typeof initialState;
